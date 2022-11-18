b37cad2cfaa7aacf22ce284986a4145317124222
Ranit Sarkar <103903179+PikuSpeaks123@users.noreply.github.com>
2022-11-18 20:07:20 +0530

    Add files via upload

---
+++ F_DIST.C
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<math.h>
+//#include<time.h>
+
+void main()
+{
+  int n,m1,m2,i,j;
+  float x,y,u,z,w,pi,v[100];
+  clrscr();
+  printf("\nEnter the sample size n : ");
+  scanf("%d",&n);
+  printf("\nEnter the Degrees of Freedom : ");
+  scanf("%d%d",&m1,&m2);
+  //srand(time(NULL));
+  randomize();
+  pi=(float)22/7;
+  for(i=0;i<n;i++)
+  {
+    z=0.0;
+    for(j=0;j<m1;j++)
+    {
+      x=rand()/(float)RAND_MAX;
+      y=rand()/(float)RAND_MAX;
+      u=sqrt(-2*log(x))*cos(2*pi*y);//Generate random no from N(0,1)
+      z=z+pow(u,2);
+    }
+    w=0.0;
+    for(j=0;j<m2;j++)
+    {
+      x=rand()/(float)RAND_MAX;
+      y=rand()/(float)RAND_MAX;
+      u=sqrt(-2*log(x))*cos(2*pi*y);//Generate random no from N(0,1)
+      w=w+pow(u,2);
+    }
+    v[i]=(float)(m2/m1)*z/w;
+  }
+  printf("\nThe random numbers are : \n");
+  for(i=0;i<n;i++)
+  {
+    printf("%f  ",v[i]);
+  }
+  getch();
+}
